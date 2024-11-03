23757bd4154bee6e53339a1c13ea2c9647a29816
ShokhjahonNosirov <shokhjahonnosirov@gmail.com>
2024-11-03 19:12:23 +0500

    new project

---
+++ static/js/scripts.js
@@ -0,0 +1,6 @@
+// Optional JavaScript if needed for form handling, e.g., form validation or submission
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Add custom validation or form submission code here
+    alert('Taksi qidiruvni boshlaymiz!');
+});
